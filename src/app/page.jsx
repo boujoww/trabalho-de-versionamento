42c7877f7f9bd555ad18bf35684de9e87ab027f3
boujoww <davimsantos09134@gmail.com>
2025-07-07 23:29:13 -0300

    navegação dos itens da pagina inicial

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,32 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24">
+        <h1 className="text-2xl font-bold text-center mb-6">
           Seja bem-vindo ao Trabalho de Versionamento de Software!
-          <br />
         </h1>
-        <p>
-          Todas as informações necessárias realização do trabalho encontra-se no
-          arquivo <span className="">README.md</span>
-        </p>
+
+        <div className="h-6" />
+
+        <nav className="flex flex-wrap gap-6 justify-center">
+          <Link href="/contato" className="text-yellow-400 hover:underline">
+            Contatos
+          </Link>
+          <Link href="/produtos" className="text-grey-100 hover:underline">
+            Produtos
+          </Link>
+          <Link href="/clientes" className="text-yellow-400 hover:underline">
+            Clientes
+          </Link>
+          <Link href="/sobre-nos" className="text-grey-100 hover:underline">
+            Sobre Nós
+          </Link>
+          <Link href="/eventos" className="text-yellow-400 hover:underline">
+            Eventos
+          </Link>
+        </nav>
       </main>
     </>
   );
